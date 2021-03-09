24ac31216ed2db7aef9f82734a0fd843cd31b206
BrodyBrosius <mcochran477@gmail.com>
2021-03-08 19:43:46 -0500

    -Imported Selenium -Utilized web driver (firefox) to input text into form and submit. -Changed direction of project to "Who are your reps?"

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
-print("hello world")
-print("yo!")
+# import libraries
+from selenium import webdriver
+
+
+web = webdriver.Firefox()
+web.get('https://www.house.gov/representatives/find-your-representative')
+zipCode = "30101"
+findZip = web.find_element_by_xpath('//*[@id="Find_Rep_by_Zipcode"]')
+findZip.send_keys(zipCode)
+submitButton = web.find_element_by_xpath('//*[@id="submit"]')
+submitButton.click()
