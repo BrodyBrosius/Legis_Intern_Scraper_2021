f5b5048ab1c2179b8afb9ccad85d91d80c8cfc45
BrodyBrosius <mcochran477@gmail.com>
2021-03-12 20:15:28 -0500

    -Commented out initial web scraping segment -Started programming function to splice user-inputted strings (their addresses) to divide the singular input string into multiple strings. Such as their state, home address, zip code and city.

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
 # import libraries
 from selenium import webdriver
 
+def parseAddress(address):
+    length = len(address)
+    userStreetAddress = ''
+    userCity = ''
+    userState = ''
+    userZipCode = ''
+    for i, character in enumerate(address):
+        if character == ',':
+            userStreetAddress = (address[0:i])
+            print(userStreetAddress)
 
-web = webdriver.Firefox()
-web.get('https://www.house.gov/representatives/find-your-representative')
-zipCode = "30101"
-findZip = web.find_element_by_xpath('//*[@id="Find_Rep_by_Zipcode"]')
-findZip.send_keys(zipCode)
-submitButton = web.find_element_by_xpath('//*[@id="submit"]')
-submitButton.click()
+
+
+
+print("Welcome to Find Your Reps!")
+userAddress = input("Please input your address. For example: 555 Example Drive, Big Town, GA, 12345")
+parseAddress(userAddress)
+
+
+
+
+
+
+
+
+
+#web = webdriver.Firefox()
+#web.get('https://www.house.gov/representatives/find-your-representative')
+#zipCode = "30101"
+#findZip = web.find_element_by_xpath('//*[@id="Find_Rep_by_Zipcode"]')
+#findZip.send_keys(zipCode)
+#submitButton = web.find_element_by_xpath('//*[@id="submit"]')
+#submitButton.click()
